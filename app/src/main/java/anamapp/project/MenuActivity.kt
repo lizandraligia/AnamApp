32e7535f99760cf8015e3abb88904934e715b921
Luks753 <lucas753159@hotmail.com.br>
2018-11-08 13:29:19 -0200

    Add files via upload

---
+++ app/src/main/java/anamapp/project/MenuActivity.kt
@@ -0,0 +1,12 @@
+package anamapp.project
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MenuActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+    }
+}
