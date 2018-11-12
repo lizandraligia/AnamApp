801c7cd393c4ed24fe4677ff05211fb153ad1c9b
Luks753 <lucas753159@hotmail.com.br>
2018-11-12 13:28:15 -0200

    Add files via upload

---
+++ app/src/main/java/anamapp/project/PatientActivity.kt
@@ -0,0 +1,14 @@
+package anamapp.project
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_patient.*
+
+class PatientActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_patient)
+
+    }
+}
