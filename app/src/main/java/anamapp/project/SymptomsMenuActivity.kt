4bab541d869fc8284e179d2a08a7c9a885a65334
christianlira98 <christianlira98@gmail.com>
2018-11-13 10:18:05 -0200

    tentando...

---
+++ app/src/main/java/anamapp/project/SymptomsMenuActivity.kt
@@ -0,0 +1,12 @@
+package anamapp.project
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SymptomsMenuActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_symptoms_menu)
+    }
+}
