ea7f6679c54cffc402cd0bb401fd64951a173397
lizandraligia <lizandraligia@hotmail.com>
2018-11-17 23:36:53 -0300

    telas não prontas ainda

---
+++ app/src/main/java/anamapp/project/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package anamapp.project
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.sign_up)
+    }
+}
