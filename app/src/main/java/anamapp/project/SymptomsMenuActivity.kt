7b6ddbdbe50cc9340277456a94544627388a6561
Luks753 <lucas753159@hotmail.com.br>
2018-11-20 17:37:17 -0200

    Activities

--- app/src/main/java/anamapp/project/SymptomsMenuActivity.kt
+++ app/src/main/java/anamapp/project/SymptomsMenuActivity.kt
@@ -3,10 +3,12 @@ package anamapp.project
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Intent
+import android.support.v4.app.FragmentManager
 import kotlinx.android.synthetic.main.activity_menu.*
 import kotlinx.android.synthetic.main.activity_symptoms_menu.*
 
 class SymptomsMenuActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,62 +23,60 @@ class SymptomsMenuActivity : AppCompatActivity() {
         menu_image_view_back3.setOnClickListener {
             finish()
         }
-
         btnGeneralSymptoms.setOnClickListener {
             val intent = Intent(applicationContext, CheckList::class.java)
+            intent.putExtra("sintoma", btnGeneralSymptoms.text.toString())
             startActivity(intent)
         }
-
         btnHeadAndNeckSymptoms.setOnClickListener {
             val intent = Intent(applicationContext, CheckList::class.java)
+            intent.putExtra("sintoma", btnHeadAndNeckSymptoms.text.toString())
             startActivity(intent)
         }
-
         btnEyeSymptoms.setOnClickListener {
             val intent = Intent(applicationContext, CheckList::class.java)
+            intent.putExtra("sintoma", btnEyeSymptoms.text.toString())
             startActivity(intent)
         }
-
         btnEarSymptoms.setOnClickListener {
             val intent = Intent(applicationContext, CheckList::class.java)
+            intent.putExtra("sintoma", btnEarSymptoms.text.toString())
             startActivity(intent)
         }
-
         btnNoseSymptoms.setOnClickListener {
             val intent = Intent(applicationContext, CheckList::class.java)
+            intent.putExtra("sintoma", btnNoseSymptoms.text.toString())
             startActivity(intent)
         }
-
         btnRespiratorySystemSymptoms.setOnClickListener {
             val intent = Intent(applicationContext, CheckList::class.java)
+            intent.putExtra("sintoma", btnRespiratorySystemSymptoms.text.toString())
             startActivity(intent)
         }
-
         btnGenitourinarySystemSymptoms.setOnClickListener {
             val intent = Intent(applicationContext, CheckList::class.java)
+            intent.putExtra("sintoma", btnGenitourinarySystemSymptoms.text.toString())
             startActivity(intent)
         }
-
         btnGastrointestinalSystemSymptoms.setOnClickListener {
             val intent = Intent(applicationContext, CheckList::class.java)
+            intent.putExtra("sintoma", btnGastrointestinalSystemSymptoms.text.toString())
             startActivity(intent)
         }
-
         btnLocomotorSystemSymptoms.setOnClickListener {
             val intent = Intent(applicationContext, CheckList::class.java)
+            intent.putExtra("sintoma", btnLocomotorSystemSymptoms.text.toString())
             startActivity(intent)
         }
-
         btnNervousSystem.setOnClickListener {
             val intent = Intent(applicationContext, CheckList::class.java)
+            intent.putExtra("sintoma", btnNervousSystem.text.toString())
             startActivity(intent)
         }
-
         btnPsycheSymptoms.setOnClickListener {
             val intent = Intent(applicationContext, CheckList::class.java)
+            intent.putExtra("sintoma", btnPsycheSymptoms.text.toString())
             startActivity(intent)
         }
     }
-
-
 }
