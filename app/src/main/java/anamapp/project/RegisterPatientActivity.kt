35a8ee25c0213132d4e5b0be7d1d51957b27491c
Jonas Freire <jonasfreireperson@gmail.com>
2018-11-20 23:18:36 -0300

    Criacao de tela Register Patient e sua variante landscape

---
+++ app/src/main/java/anamapp/project/RegisterPatientActivity.kt
@@ -0,0 +1,12 @@
+package anamapp.project
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterPatientActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_patient)
+    }
+}
