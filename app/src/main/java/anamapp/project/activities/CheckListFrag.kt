f5d03c98184219a894e82cee26f28963fd19fbb3
lizandraligia <lizandraligia@hotmail.com>
2019-01-13 22:07:24 -0300

    fragment quase funcionando

--- app/src/main/java/anamapp/project/activities/CheckListFrag.kt
+++ app/src/main/java/anamapp/project/activities/CheckListFrag.kt
@@ -1,22 +1,39 @@
 package anamapp.project.activities
 
 import anamapp.project.R
+import anamapp.project.bean.Constant
+import android.content.Intent
 import android.os.Bundle
-import android.app.Fragment
+import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import android.widget.ScrollView
+import org.json.JSONArray
+import org.json.JSONObject
+import kotlinx.android.synthetic.main.fragment_check_list.*
 
 
 class CheckListFrag : Fragment() {
 
 
+    companion object {
+
+        fun newInstance(): CheckListFrag {
+            return CheckListFrag()
+        }
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle): View? {
-        return inflater.inflate(R.layout.fragment_check_list, container, false)
-    }
+                             savedInstanceState: Bundle?): View? {
+
+       var rootView : View = inflater.inflate(R.layout.activity_check_list, container, false)
 
 
-    }
+       return rootView
 
+       //return inflater?.inflate(R.layout.activity_check_list, container, false)
+   }
+
+}
